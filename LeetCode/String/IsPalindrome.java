7faf8e6d0fc1e3fcdc8a8b163e88ee78acb1fe3c
Suchishree Jena <suchishree.jena29@gmail.com>
2018-01-20 15:49:19 -0800

    Update IsPalindrome.java

--- LeetCode/String/IsPalindrome.java
+++ LeetCode/String/IsPalindrome.java
@@ -1,1 +1,47 @@
-125. Valid Palindrome
+/*125. Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.*/
+
+package string;
+
+public class IsPalindrome {
+	public boolean isPalindrome(String s) {
+		
+		if(s.length() == 0){
+			return true;
+		}
+		
+		int i = 0;
+		int j = s.length()-1;
+		s = s.toUpperCase();
+		
+		while(i<j){
+			if(s.charAt(i) == s.charAt(j) && Character.isLetterOrDigit(s.charAt(i))){
+					i++;
+					j--;
+			}else if(!Character.isLetterOrDigit(s.charAt(i))){
+				i++;
+			}else if(!Character.isLetterOrDigit(s.charAt(j))){
+				j--;
+			}else{
+				return false;
+				
+			}
+		}
+       return true; 
+    }
+	
+	public static void main(String[] args) {
+		IsPalindrome p = new IsPalindrome();
+		System.out.println(p.isPalindrome("A man, a plan, a canal: Panama"));
+	}
+
+}
